44cd0053c8fb1f412e67b0ec3b54b3b4e90026f1
sobitd <sobitdaniel@gmail.com>
2023-03-08 10:39:43 +0300

    Add user.service.js

---
+++ src/services/user.service.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const api = "http://localhost:5000/api/protected/";
+
+const getPublicContent = () => {
+  return axios.get(api + "public");
+};
+
+const getUserBoard = () => {
+  return axios.get(api + "user");
+};
+
+const getEditorBoard = () => {
+  return axios.get(api + "editor");
+};
+
+const getSupervisorBoard = () => {
+  return axios.get(api + "supervisor");
+};
+
+const getPrincipalBoard = () => {
+  return axios.get(api + "principal");
+};
+
+const getAdminBoard = () => {
+  return axios.get(api + "admin");
+};
+
+const UserService = {
+  getUserBoard,
+  getEditorBoard,
+  getSupervisorBoard,
+  getPrincipalBoard,
+  getAdminBoard,
+};
+
+export default UserService;
